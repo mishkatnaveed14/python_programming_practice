7103ac00709303be92089010fd807f4d5ce392e0
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-24 13:35:35 +0500

    working on numpy task

---
+++ Numpy/nump-practice-task.py
@@ -0,0 +1,16 @@
+import numpy as np
+# task ===> 1
+array = np.array([10,20,30,40,50])
+print(array.sum())
+print(array.mean())
+print(array.max())
+print(array.min())  
+
+# task ===> 2
+array2 = np.array([45,80,30,90,55,20])
+print(array2[array2 >= 50])
+# task ==>  3
+array3 = np.array([1,2,3,4,5,6])
+new_array = array3.reshape(2,3)
+print(new_array)
+# task ==> 4
