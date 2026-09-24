0e87fb003cd2bdffc8707ba515442774cdc5894b
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-24 18:26:21 +0500

    5 tasks are done !!

--- Numpy/numpy-practice-task.py
+++ Numpy/numpy-practice-task.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 # task ===> 1
 array = np.array([10, 20, 30, 40, 50])
 print(array.sum())
@@ -10,10 +9,12 @@ print(array.min())
 # task ===> 2
 array2 = np.array([45, 80, 30, 90, 55, 20])
 print(array2[array2 >= 50])
+
 # task ==>  3
 array3 = np.array([1, 2, 3, 4, 5, 6])
 new_array = array3.reshape(2, 3)
 print(new_array)
+
 # task ==> 4
 marks = np.random.randint(40, 100, 10)
 average = marks.mean()
